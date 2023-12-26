cdfd655cb8abd618eca5cd18933277ac0fd613e6
Javier Valero Cejudo <javalce29@gmail.com>
2023-12-26 19:48:44 +0100

    Refactor createChristmasTree function to improve performance and readability

--- 2023/reto-10/solution.ts
+++ 2023/reto-10/solution.ts
@@ -2,27 +2,23 @@ export function createChristmasTree(ornaments: string, height: number): string {
   let tree = '';
   let ornamentIndex = 0;
 
-  function getSpaces(height: number): string {
-    return ' '.repeat(height);
-  }
-
-  function getOrnament(): string {
-    const ornament = ornaments[ornamentIndex];
-
-    ornamentIndex++;
-    ornamentIndex %= ornaments.length;
-
-    return ornament;
-  }
-
   for (let i = 1; i <= height; i++) {
-    const lateralSpaces = getSpaces(height - i);
-    const centerOrnaments = Array.from({ length: i }, getOrnament).join(' ');
+    const lateralSpaces = ' '.repeat(height - i);
+    const centerOrnamentsArray: string[] = [];
+
+    for (let j = 0; j < i; j++) {
+      const ornament = ornaments[ornamentIndex];
+
+      ornamentIndex = (ornamentIndex + 1) % ornaments.length;
+      centerOrnamentsArray.push(ornament);
+    }
+
+    const centerOrnaments = centerOrnamentsArray.join(' ');
 
     tree += `${lateralSpaces}${centerOrnaments}\n`;
   }
 
-  const baseSpaces = getSpaces(height - 1);
+  const baseSpaces = ' '.repeat(height - 1);
   const base = `${baseSpaces}|\n`;
 
   tree += base;
