4b3379bbc54a63c1f780d123a429c97b6e93aa1b
Javier Valero Cejudo <javalce29@gmail.com>
2024-12-01 15:07:21 +0100

    Refactor command-line argument handling: replace yargs with commander for improved clarity and maintainability

--- index.js
+++ index.js
@@ -2,62 +2,70 @@
 import { readdir } from 'node:fs/promises';
 import path from 'node:path';
 
+import { Command } from 'commander';
 import prompts from 'prompts';
 import { startVitest } from 'vitest/node';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 
-const args = yargs(hideBin(process.argv)).options({
-  year: {
-    type: 'string',
-    description: 'Año de los retos',
-    default: '2023',
-  },
-  challenge: {
-    type: 'string',
-    description: 'Reto a resolver',
-  },
-});
-
-prompts.override(args.argv);
+function cancelOperation() {
+  console.log('\nAdios! 👋\n');
+  process.exit(0);
+}
 
 async function main() {
-  const { year, challenge } = await prompts(
-    [
+  const program = new Command()
+    .addArgument('[year]', 'Año de los retos')
+    .addArgument('[challenge]', 'Reto a resolver');
+
+  let { year, challenge } = program.opts();
+
+  if (year === undefined) {
+    const result = await prompts(
       {
         type: 'select',
         name: 'year',
         message: '¿De qué año quieres ejecutar los tests?',
         initial: 0,
-        choices: [{ title: '2023', value: '2023' }],
+        choices: [
+          { title: '2023', value: '2023' },
+          { title: '2024', value: '2024' },
+        ],
       },
+      {
+        onCancel: cancelOperation,
+      },
+    );
+
+    year = result.year;
+  }
+
+  if (challenge === undefined) {
+    const yearDir = path.join(process.cwd(), year);
+    const challenges = await readdir(yearDir);
+
+    const result = await prompts(
       {
         type: 'select',
         name: 'challenge',
         message: '¿De qué reto quieres ejecutar los tests?',
         initial: 0,
-        choices: async (year) => {
-          const yearDir = path.join(process.cwd(), year);
-          const challenges = await readdir(yearDir);
-          const challengesChoices = challenges.map((challenge) => {
+        choices: [
+          { title: 'Ejecutar todos los tests', value: 'all' },
+          ...challenges.map((challenge) => {
             let title = challenge.replace(/-/g, ' ');
 
             title = title[0].toUpperCase() + title.slice(1);
 
             return { title, value: challenge };
-          });
+          }),
+        ],
+      },
+      {
+        onCancel: cancelOperation,
+      },
+    );
 
-          return [{ title: 'Ejecutar todos los tests', value: 'all' }, ...challengesChoices];
-        },
-      },
-    ],
-    {
-      onCancel: () => {
-        console.log('\nAdios! 👋\n');
-        process.exit(0);
-      },
-    },
-  );
+    challenge = result.challenge;
+  }
 
   const directory = path.join(year, challenge === 'all' ? '' : challenge);
 
